6c1c38b45ef348d7641dc65736e9d444ab2b9adf
adilmaqsood1 <126709358+adilmaqsood1@users.noreply.github.com>
2023-11-07 11:34:05 +0500

    MAIN

--- TIC-TAC-TOE/player_logic.py
+++ TIC-TAC-TOE/player_logic.py
@@ -35,3 +35,50 @@ class humanplayer(player):
                 print('invalid square. try again.')   
 
         return val        
+    
+class geniuscomputetrplayer(player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square =random.choice(game.available_move())  
+        else:
+            square = self.minimax(game, self.letter)    
+        return square
+            
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player ='o' if player == 'x' else 'x'
+
+        if  state.current_winner == other_player:
+            return{'position': None,
+                   'score': 1*(state.num_empty_square()+1) if other_player == max_player else -1 * (state.num_empty_squares() + 1)
+                   }
+        
+        elif not state.empty_squares():
+            return{'position': None, 'score': 0 }
+        
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf }
+
+        else:
+            {'position': None, 'score': math.inf }
+
+        for possible_move in state.available_moves():
+            state.make_move(possible_move, player)
+
+            sim_score = self.minimax(state, other_player)
+
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            sim_score ['position'] = possible_move
+
+            if player == max_player:
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+
+        return best
